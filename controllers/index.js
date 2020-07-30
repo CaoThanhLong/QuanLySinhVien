1f832f39b58993f7e655b8679dd553c56e7f3b0d
CaoThanhLong <ctlong26051993@gmail.com>
2020-07-30 19:15:14 +0700

    uploadsource

---
+++ controllers/index.js
@@ -0,0 +1,32 @@
+// Tạo đối tượng service tương tác backend
+var svService = new SinhVienService();
+
+var renderSinhVien = function () {
+  var promise = svService.layDanhSachSinhVien();
+  promise
+    .then(function (result) {
+      // Tạo nội dung các thẻ tr
+      var contentTable = "";
+      for (var i = 0; i < result.data.length; i++) {
+        const sinhVien = result.data[i];
+        contentTable += `
+            <tr> 
+                <td>${sinhVien.MaSV}</td>
+                <td>${sinhVien.HoTen}</td>
+                <td>${sinhVien.Email}</td>
+                <td>${sinhVien.SoDT}</td>
+                <td>${sinhVien.DiemToan}</td>
+                <td>${sinhVien.DiemLy}</td>
+                <td>${sinhVien.DiemHoa}</td>
+            </tr>`;
+      }
+
+      // In ra giao diện
+      document.getElementById("tblSinhVien").innerHTML = contentTable;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+renderSinhVien();
